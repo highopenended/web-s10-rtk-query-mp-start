40cd4f47f278d1676e7a8cd43243c748036c2397
Matt Glenn <highopenended@gmail.com>
2024-09-25 08:24:38 -0400

    Got initial api state hooked up and partially implemented

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Quotes from './Quotes'
 import QuoteForm from './QuoteForm'
+// import { useGetQuotesQuery } from '../state/quotesApi'
 
 export default function App() {
+  // const {data:quotes} = useGetQuotesQuery
+
   return (
     <div id="mp">
       <h2>Module Project</h2>
--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,33 @@
 // create your RTK Query endpoints here
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/' }),
+    tagTypes: ['Quotes'], 
+
+    endpoints: builder => ({
+      getQuotes: builder.query({
+        query: () => 'quotes',
+      }),
+      createQuote: builder.mutation({
+        
+      }),
+      deleteQuote: builder.mutation({
+
+      }),
+      toggleFake: builder.mutation({
+
+      }),
+
+      invalidatesTags: ['Quotes'], 
+    }),
+  })
+
+  export const {
+    useGetQuotesQuery,
+    useCreateQuoteQuery,
+    useDeleteQuoteQuery,
+    useToggleFakeQuery,
+  } = quotesApi
